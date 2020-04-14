69405bc31339dc59e93e2da5374e8cdce5a315af
CrisDeveloperRepo1 <44676837+CrisDeveloperRepo1@users.noreply.github.com>
2020-04-14 20:06:00 -0300

    Update ejercicio10P2.py

--- ejercicio10P2.py
+++ ejercicio10P2.py
@@ -1,25 +1,27 @@
 str = ['im1','im2','im3']
 cont=0
-
-
-
-
 for i in str:
-  print('ingrese una coordenada')
+  print('ingrese')
   caracter= input()
 
   if cont==0 :
 	  str[cont] = i+" "+ caracter
-
   
-	#coord = input()
+  elif cont>1 and cont<=len(str)-1:
+	  while(-1 !=(str[cont-1].find(caracter)))or (-1 !=(str[cont-2].find(caracter))) :
+		  print('ingrese otra coord repetida')
+		  caracter=input()	
   else : 	
     while(-1 !=(str[cont-1].find(caracter))):
-      print('coord repetida')
+      print('ingrese otra coord repetida')
       caracter= input()	
   str[cont] = i+" "+ caracter
-  cont = cont + 1
-      
+  
+  cont = cont+1    
     	
 		
-print(str)	
+print(str)		 
+		
+
+##### no me convence como quedo apesar de que hace lo que pide el ejercicio , me gustaria tener una solucion mucho mas simple y no  usar tres condicionales 	
+######	
